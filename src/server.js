65a1f61c8234300f43091ab7f4887dcff142ae45
Kenneth <kenneth.youthmail@gmail.com>
2020-08-17 19:34:43 +0800

    CORS PROXY

--- src/server.js
+++ src/server.js
@@ -1,10 +1,15 @@
-
 const express = require('express')
 const cors = require('cors');
 const app = express()
 const port = 5000
 
-app.use(cors());
+app.use(cors({ origin: true }))
+
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.get('/test', (req, res) => {
   res.send('Hello World!')
